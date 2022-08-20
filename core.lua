78554bd739209d70101320ce0635aca3c45f3107
Sam Deane <sam@elegantchaos.com>
2022-08-20 16:40:19 +0100

    Ported element handling

--- core.lua
+++ core.lua
@@ -1,3 +1,8 @@
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+--  Created by Samedi on 20/08/2022.
+--  All code (c) 2022, The Samedi Corporation.
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 ModulaCore = {
 }
 
@@ -6,6 +11,7 @@ function ModulaCore.new(system, library, player, construct, unit, settings)
     local instance = {
         _modules = {},
         _actions = {},
+        _elements = {},
         _state = {},
         _skipLoader = settings.devMode or false,
         _logging = settings.logging or false,
@@ -17,10 +23,16 @@ function ModulaCore.new(system, library, player, construct, unit, settings)
     setmetatable(instance, { __index = ModulaCore })
 
     instance:setupGlobals(system, library, player, construct, unit)
-    debug("modula: initialised")
+    instance:loadElements()
+
+    debug("Initialised Modula Core")
     
     return instance
 end
+
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+-- Event Handlers
+-- -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 function ModulaCore:onStart()
 end
@@ -45,6 +57,93 @@ end
 
 function ModulaCore:onInput(text)
 end
+
+
+-- ---------------------------------------------------------------------
+-- Elements
+-- ---------------------------------------------------------------------
+
+function ModulaCore:loadElements()
+    local all = self:allElements()
+    local elements = self:categoriseElements(all)
+    local cores = elements.CoreUnitStatic or elements.CoreUnitDynamic or elements.CoreUnitSpace
+    if cores and (#cores > 0) then
+        self._core = cores[1]
+    else
+        error("Core not found. Need to link the core to the controller.")
+    end
+
+    self._elements = elements
+    self._settings = self:findElement("DataBankUnit")
+
+    local core = self._core
+    self:withElements("ScreenUnit", function(element)
+        local id = element.getId()
+        if core.getElementNameById(id) == "Console" then
+            debug("Installed console.")
+            self._console = element
+        end
+    end)
+end
+
+function ModulaCore:allElements()
+    local elements = {}
+    for k,v in pairs(_G) do
+        if (k:find("Unit_") == 1) and (v.getElementClass) then
+            table.insert(elements, v)
+        end
+    end
+    return elements
+end
+
+function ModulaCore:categoriseElements(elements)
+    local categorised = {}
+    for i,element in ipairs(elements) do
+        local class = element.getClass()
+        local classElements = categorised[class]
+        if not classElements then
+            classElements = {}
+            categorised[class] = classElements
+        end
+
+        table.insert(classElements, element)
+    end
+
+    if self._logElements then
+        for k,v in pairs(categorised) do
+            debug("Found %s %s.", #v, k)
+        end
+    end            
+
+    categorised.all = elements
+    return categorised
+end
+
+function ModulaCore:findElement(category,  action)
+    local list = self._elements[category]
+    if list then
+        local element = list[1]
+        if action then
+            action(element)
+        end
+        return element
+    end
+end
+
+function ModulaCore:withElements(category, action)
+    local elements = self._elements[category]
+    if elements then
+        for i,element in ipairs(elements) do
+            action(element, i)
+        end
+    end
+end
+
+
+-- ---------------------------------------------------------------------
+-- Internal
+-- ---------------------------------------------------------------------
+
 
 function ModulaCore:setupGlobals(system, library, player, construct, unit)
     _G.system = _G.system or system
