0a524ab359c17fcaad4b58554d7a4b5035afea2f
Sam Deane <sam@elegantchaos.com>
2022-08-20 15:47:35 +0100

    added global print

--- core.lua
+++ core.lua
@@ -1,15 +1,9 @@
-
 ModulaCore = {
 }
 
 function ModulaCore.new(system, library, player, construct, unit, settings)
     settings = settings or {}
     local instance = {
-        system = system,
-        library = library,
-        player = player,
-        construct = construct,
-        unit = unit,
         _modules = {},
         _actions = {},
         _state = {},
@@ -22,8 +16,9 @@ function ModulaCore.new(system, library, player, construct, unit, settings)
 
     setmetatable(instance, { __index = ModulaCore })
 
-    instance:log("started")
-
+    instance:setupGlobals(system, library, player, construct, unit)
+    print("started")
+    
     return instance
 end
 
@@ -51,8 +46,49 @@ end
 function ModulaCore:onInput(text)
 end
 
+function ModulaCore:setupGlobals(system, library, player, construct, unit)
+    _G.system = _G.system or system
+    _G.unit = _G.unit or unit
+    _G.library = _G.library or library
+    _G.player = _G.player or player
+    _G.construct = _G.construct or construct
+
+    _G.print = function(format, ...)
+        if type(format) == "table" then
+            for k,v in pairs(format) do
+                system.print(string.format("%s: %s", k, v))
+            end
+        else
+            system.print(format:format(...))
+        end
+    end
+
+    -- if self._logging then
+    --     _G.debug = _G.print
+    -- else
+    --     _G.debug = function() end
+    -- end
+
+    system.print("blah")
+
+    -- _G.logf = function(s,...)
+    --     local message = s:format(...) 
+    --     system.logInfo(string.format("§±%s±§", message))
+    -- end
+
+    -- _G.warning = function(s, ...)
+    --     printf("WARNING: %s", s:format(...))
+    -- end
+
+    -- _G.fail = function(s, ...)
+    --     local message = s:format(...)
+    --     system.showScreen(1)
+    --     system.setScreen(string.format('<div class="window" style="position: absolute; top="10vh"; left="45vw"; width="10vw"><h1 style="middle">Error</h1><span>%s</span></div>', message))
+    -- end
+end
+
 function ModulaCore:log(item)
-    self.system.print(item)
+    print(item)
 end
 
 return ModulaCore
