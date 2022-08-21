ebb5b70de4e89cff7972fdf14810ee3ce29df7cd
Sam Deane <sam@elegantchaos.com>
2022-08-21 13:31:10 +0100

    don't use _ for local vars

--- core.lua
+++ core.lua
@@ -9,20 +9,20 @@ ModulaCore = {
 function ModulaCore.new(system, library, player, construct, unit, settings)
     settings = settings or {}
     local instance = {
-        _name = "core",
-        _constructName = settings.name or "Untitled Construct",
-        _modules = settings.modules or {},
-        _moduleNames = {},
-        _moduleIndex = {},
-        _actions = {},
-        _elements = {},
-        _state = {},
-        _skipLocal = settings.devMode or false,
-        _logging = settings.logging or false,
-        _logElements = settings.logElements or false,
-        _logActions = settings.logActions or false,
-        _timers = {},
-        _handlers = {}
+        name = "core",
+        constructName = settings.name or "Untitled Construct",
+        modules = settings.modules or {},
+        moduleNames = {},
+        moduleIndex = {},
+        actions = {},
+        elements = {},
+        state = {},
+        useLocal = settings.useLocal or false,
+        logging = settings.logging or false,
+        logElements = settings.logElements or false,
+        logActions = settings.logActions or false,
+        timers = {},
+        handlers = {}
     }
 
     setmetatable(instance, {__index = ModulaCore})
@@ -42,7 +42,7 @@ end
 -- ---------------------------------------------------------------------
 
 function ModulaCore:setupHandlers()
-    self._handlers = {
+    self.handlers = {
         onStart = { self },
         onStop = {},
         onActionStart = {},
@@ -55,7 +55,7 @@ function ModulaCore:setupHandlers()
 end
 
 function ModulaCore:call(handler, ...)
-    local objects = self._handlers[handler]
+    local objects = self.handlers[handler]
     if not objects then
         return
     end
@@ -79,7 +79,7 @@ function ModulaCore:registerForEvents(handlers, object)
             warning("Module %s does not have a handler for %s", object.name, handler)
         else
             debug("Registering %s for event %s", object.name, handler)
-            local registered = self._handlers[handler]
+            local registered = self.handlers[handler]
             if registered then
                 table.insert(registered, object)
             else
@@ -98,7 +98,7 @@ end
 -- ---------------------------------------------------------------------
 
 function ModulaCore:registerModules()
-    for module, parameters in pairs(self._modules or {}) do
+    for module, parameters in pairs(self.modules or {}) do
         self:registerModule(module, parameters)
     end
 end
@@ -110,9 +110,9 @@ function ModulaCore:registerModule(name, parameters)
         local module = {}
         setmetatable(module, { __index = prototype })
 
-        table.insert(self._modules, module)
-        table.insert(self._moduleNames, name)
-        self._moduleIndex[name] = module
+        table.insert(self.modules, module)
+        table.insert(self.moduleNames, name)
+        self.moduleIndex[name] = module
 
         module:register(self, parameters)
     else
@@ -124,15 +124,22 @@ end
 function ModulaCore:loadModule(name)
     local module
 
-    -- if not self._skipLocal then
+    if self.useLocal then 
+        -- prefer local source version if it is present
+        module = require(string.format(name)) 
+    end
+
+    if not module then
+        -- try to load module from version stashed in library.onStart
         local loaderName = name:gsub("[.-]", "_")
         local loader = _G[string.format("MODULE_%s", loaderName)]
         module = loader()
-    -- end
+    end
 
-    if not module then module = require(string.format(name)) end
+    if module then
+        module.name = name
+    end
 
-    module.name = name
     return module
 end
 
@@ -146,20 +153,20 @@ function ModulaCore:loadElements()
     local cores = elements.CoreUnitStatic or elements.CoreUnitDynamic or
                       elements.CoreUnitSpace
     if cores and (#cores > 0) then
-        self._core = cores[1]
+        self.core = cores[1]
     else
         error("Core not found. Need to link the core to the controller.")
     end
 
-    self._elements = elements
-    self._settings = self:findElement("DataBankUnit")
+    self.elements = elements
+    self.settings = self:findElement("DataBankUnit")
 
-    local core = self._core
+    local core = self.core
     self:withElements("ScreenUnit", function(element)
         local id = element.getId()
         if core.getElementNameById(id) == "Console" then
             debug("Installed console.")
-            self._console = element
+            self.console = element
         end
     end)
 end
@@ -187,7 +194,7 @@ function ModulaCore:categoriseElements(elements)
         table.insert(classElements, element)
     end
 
-    if self._logElements then
+    if self.logElements then
         for k, v in pairs(categorised) do debug("Found %s %s.", #v, k) end
     end
 
@@ -196,7 +203,7 @@ function ModulaCore:categoriseElements(elements)
 end
 
 function ModulaCore:findElement(category, action)
-    local list = self._elements[category]
+    local list = self.elements[category]
     if list then
         local element = list[1]
         if action then action(element) end
@@ -205,7 +212,7 @@ function ModulaCore:findElement(category, action)
 end
 
 function ModulaCore:withElements(category, action)
-    local elements = self._elements[category]
+    local elements = self.elements[category]
     if elements then
         for i, element in ipairs(elements) do action(element, i) end
     end
