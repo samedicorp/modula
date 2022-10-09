c8ac38275ce28376ada26844d73b275b9104e0b1
Sam Deane <sam@elegantchaos.com>
2022-10-09 15:24:32 +0100

    basic info

--- modules/industry.lua
+++ modules/industry.lua
@@ -6,7 +6,6 @@
 -- This module tracks any industry units connected to the core.
 
 local Module = { }
-local Industry = {}
 
 function Module:register(parameters)
     parameters = parameters or {}
@@ -22,31 +21,66 @@ end
 -- ---------------------------------------------------------------------
 
 
+-- ---------------------------------------------------------------------
+-- Public API
+-- ---------------------------------------------------------------------
+
+function Module:getMachines()
+    return self.industry
+end
 
 -- ---------------------------------------------------------------------
 -- Internal
 -- ---------------------------------------------------------------------
 
+local Machine = {}
+local Product = {}
+
 function Module:findIndustry()
     local industry = {}
     local core = modula.core
     local elementIDs = core.getElementIdList()
-    for elementID in ipairs(elementIDs) do
+    for i,elementID in ipairs(elementIDs) do
         local class = core.getElementClassById(elementID)
         if class == "Industry1" then
+            local info = core.getElementIndustryInfoById(elementID)
             local item = { 
                 localID = elementID,
                 class = class,
                 name = core.getElementDisplayNameById(elementID),
-                label = core.getElementNameById(elementID)
+                label = core.getElementNameById(elementID),
+                info = info
             }
-            setmetatable(item, { __index = Industry })
 
+            local productInfos = {}
+            for n, productInfo in pairs(info.currentProducts) do
+                local product = { 
+                    id = productInfo.id,
+                    quantity = productInfo.quantity
+                }
+                product.info = system.getItem(productInfo.id)
+                setmetatable(product, { __index = Product })
+                table.insert(productInfos, product)
+            end
+
+            item.products = productInfos
+            item.mainProduct = productInfos[1]
+
+            setmetatable(item, { __index = Machine })
             table.insert(industry, item)
         end
     end
 
     self.industry = industry
 end
+
+function Product:getName()
+    return self.info.locDisplayName
+end
+
+function Product:getIcon()
+    return self.info.iconPath
+end
+
 
 return Module
