3ccd0a903439a5932a461b23ae8139dca629aad6
Sam Deane <sam@elegantchaos.com>
2022-10-09 15:40:15 +0100

    Simplified machine structure

--- modules/industry.lua
+++ modules/industry.lua
@@ -10,8 +10,6 @@ local Module = { }
 function Module:register(parameters)
     parameters = parameters or {}
 
-    self.industry = {}
-
     modula:registerService(self, "industry")
     self:findIndustry()
 end
@@ -26,7 +24,7 @@ end
 -- ---------------------------------------------------------------------
 
 function Module:getMachines()
-    return self.industry
+    return self.machines
 end
 
 -- ---------------------------------------------------------------------
@@ -37,23 +35,20 @@ local Machine = {}
 local Product = {}
 
 function Module:findIndustry()
-    local industry = {}
+    local machines = {}
     local core = modula.core
     local elementIDs = core.getElementIdList()
     for i,elementID in ipairs(elementIDs) do
         local class = core.getElementClassById(elementID)
-        if class == "Industry1" then
-            local info = core.getElementIndustryInfoById(elementID)
-            local item = { 
-                localID = elementID,
-                class = class,
-                name = core.getElementDisplayNameById(elementID),
-                label = core.getElementNameById(elementID),
-                info = info
-            }
+        if class:find("Industry", 1, true) == 1 then
+            local machine = core.getElementIndustryInfoById(elementID)
+            machine.localID = elementID
+            machine.class = class
+            machine.name = core.getElementDisplayNameById(elementID)
+            machine.label = core.getElementNameById(elementID)
 
             local productInfos = {}
-            for n, productInfo in pairs(info.currentProducts) do
+            for n, productInfo in pairs(machine.currentProducts) do
                 local product = { 
                     id = productInfo.id,
                     quantity = productInfo.quantity
@@ -63,15 +58,43 @@ function Module:findIndustry()
                 table.insert(productInfos, product)
             end
 
-            item.products = productInfos
-            item.mainProduct = productInfos[1]
+            machine.products = productInfos
+            machine.mainProduct = productInfos[1]
 
-            setmetatable(item, { __index = Machine })
-            table.insert(industry, item)
+            setmetatable(machine, { __index = Machine })
+            table.insert(machines, machine)
         end
     end
 
-    self.industry = industry
+    self.machines = machines
+end
+
+function Machine:isStopped()
+    return self.state == 1
+end
+
+function Machine:isRunning()
+    return self.state == 2
+end
+
+function Machine:isPending()
+    return self.state == 6
+end
+
+function Machine:isFull()
+    return self.state == 4
+end
+
+function Machine:isMissingIngredients()
+    return self.state == 3
+end
+
+function Machine:isMissingSchematics()
+    return self.state == 7
+end
+
+function Machine:isMissingOutputContainer()
+    return self.state == 5
 end
 
 function Product:getName()
